72e5d5af32bb0ebe9428acee9ba83bde31f251dd
odunadeleke <odunadeleke@hotmail.com>
2018-10-01 16:06:20 +0100

    v1.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 import { AppComponent } from './app.component';
+
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+  
+    AppComponent,
+    
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    
+    BrowserModule,
+    
+    HttpModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
